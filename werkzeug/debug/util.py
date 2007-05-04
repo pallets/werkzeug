d92992e2b7b2b71b04fb23b99eb881489a1f3713
Georg Brandl <georg@python.org>
2007-05-04 22:54:01 +0200

    [svn] Move the highlighter into util.
    --HG--
    branch : trunk

--- werkzeug/debug/util.py
+++ werkzeug/debug/util.py
@@ -3,19 +3,22 @@
     werkzeug.debug.util
     ~~~~~~~~~~~~~~~~~~~
 
-    Debugging utilities.
+    Utilities for the debugger.
 
     :copyright: 2007 by Georg Brandl, Armin Ronacher.
     :license: BSD, see LICENSE for more details.
 """
 import os
+import re
 import sys
+import token
 import inspect
+import keyword
+import tokenize
 import threading
+from cgi import escape
 from random import random
 from cStringIO import StringIO
-
-from werkzeug.debug.highlighter import PythonParser
 
 
 def get_current_thread():
@@ -70,6 +73,107 @@ def get_uid():
     Return a random unique ID.
     """
     return str(random()).encode('base64')[3:11]
+
+
+class PythonParser(object):
+    """
+    Simple python sourcecode highlighter.
+
+    Usage::
+
+        p = PythonParser(source)
+        p.parse()
+        for line in p.get_html_output():
+            print line
+    """
+
+    _KEYWORD = token.NT_OFFSET + 1
+    _TEXT    = token.NT_OFFSET + 2
+    _classes = {
+        token.NUMBER:       'num',
+        token.OP:           'op',
+        token.STRING:       'str',
+        tokenize.COMMENT:   'cmt',
+        token.NAME:         'id',
+        token.ERRORTOKEN:   'error',
+        _KEYWORD:           'kw',
+        _TEXT:              'txt',
+    }
+
+    def __init__(self, raw):
+        self.raw = raw.expandtabs(8)
+        if isinstance(self.raw, unicode):
+            self.raw = self.raw.encode('utf-8', 'ignore')
+        self.out = StringIO()
+
+    def parse(self):
+        self.lines = [0, 0]
+        pos = 0
+        while 1:
+            pos = self.raw.find('\n', pos) + 1
+            if not pos:
+                break
+            self.lines.append(pos)
+        self.lines.append(len(self.raw))
+
+        self.pos = 0
+        text = StringIO(self.raw)
+        try:
+            tokenize.tokenize(text.readline, self)
+        except tokenize.TokenError:
+            pass
+
+    def get_html_output(self):
+        """ Return line generator. """
+        def html_splitlines(lines):
+            # this cool function was taken from trac.
+            # http://projects.edgewall.com/trac/
+            open_tag_re = re.compile(r'<(\w+)(\s.*)?[^/]?>')
+            close_tag_re = re.compile(r'</(\w+)>')
+            open_tags = []
+            for line in lines:
+                for tag in open_tags:
+                    line = tag.group(0) + line
+                open_tags = []
+                for tag in open_tag_re.finditer(line):
+                    open_tags.append(tag)
+                open_tags.reverse()
+                for ctag in close_tag_re.finditer(line):
+                    for otag in open_tags:
+                        if otag.group(1) == ctag.group(1):
+                            open_tags.remove(otag)
+                            break
+                for tag in open_tags:
+                    line += '</%s>' % tag.group(1)
+                yield line
+
+        return list(html_splitlines(self.out.getvalue().splitlines()))
+
+    def __call__(self, toktype, toktext, (srow,scol), (erow,ecol), line):
+        oldpos = self.pos
+        newpos = self.lines[srow] + scol
+        self.pos = newpos + len(toktext)
+
+        if toktype in [token.NEWLINE, tokenize.NL]:
+            self.out.write('\n')
+            return
+
+        if newpos > oldpos:
+            self.out.write(self.raw[oldpos:newpos])
+
+        if toktype in [token.INDENT, token.DEDENT]:
+            self.pos = newpos
+            return
+
+        if token.LPAR <= toktype and toktype <= token.OP:
+            toktype = token.OP
+        elif toktype == token.NAME and keyword.iskeyword(toktext):
+            toktype = self._KEYWORD
+        clsname = self._classes.get(toktype, 'txt')
+
+        self.out.write('<span class="code-item p-%s">' % clsname)
+        self.out.write(escape(toktext))
+        self.out.write('</span>')
 
 
 def get_frame_info(tb, context_lines=7):
