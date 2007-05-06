77ff915ba8e85f6366c6e96c30578055381498bc
Armin Ronacher <armin.ronacher@active-4.com>
2007-05-06 20:48:08 +0200

    [svn] added a routing example file (not complex by now)
    --HG--
    branch : trunk

---
+++ examples/complex_routing.py
@@ -0,0 +1,19 @@
+from werkzeug.routing import Map, Rule, Subdomain, Submount, EndpointPrefix
+
+m = Map([
+    # Static URLs
+    EndpointPrefix('static/', [
+        Rule('/', endpoint='index'),
+        Rule('/about', endpoint='about'),
+        Rule('/help', endpoint='help'),
+    ]),
+    # Knowledge Base
+    Subdomain('kb', [EndpointPrefix('kb/', [
+        Rule('/', endpoint='index'),
+        Submount('/browse', [
+            Rule('/', endpoint='browse'),
+            Rule('/<int:id>/', screen={'page': 1}, endpoint='browse'),
+            Rule('/<int:id>/<int:page>', endpoint='browse')
+        ])
+    ])])
+], redirect_screened=True)
