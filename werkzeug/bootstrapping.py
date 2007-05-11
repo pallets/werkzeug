aefd31055b281b28dec1dc16e06cb47b09f34cdd
Armin Ronacher <armin.ronacher@active-4.com>
2007-05-12 01:01:33 +0200

    [svn] documented bootstrapping
    --HG--
    branch : trunk

--- werkzeug/bootstrapping.py
+++ werkzeug/bootstrapping.py
@@ -11,11 +11,13 @@
 """
 import os
 import sys
+from fnmatch import fnmatch
 from getopt import getopt, GetoptError
 from werkzeug.minitmpl import Template
 
 
 TEMPLATES = os.path.join(os.path.dirname(__file__), 'templates')
+TEMPLATED_FILTERS = ['*.py', '*.html', '*.txt']
 
 
 def bootstrap(package_name, destination_path, template, charset):
@@ -26,10 +28,28 @@ def bootstrap(package_name, destination_path, template, charset):
         template_path = os.path.join(TEMPLATES, template)
     else:
         template_path = template
+
+    if not os.path.exists(template_path):
+        print >>sys.stderr, "Template path not found"
+        return -2
+
     pascal_cased = package_name.title().replace('_', '')
 
     print 'Generating from "%s"' % template_path
 
+    templated_fn = os.path.join(template_path, '.TEMPLATED')
+    templated_filters = []
+    if os.path.exists(templated_fn):
+        f = file(templated_fn)
+        try:
+            for line in f:
+                templated_filters.append(line.strip())
+        finally:
+            f.close()
+    else:
+        templated_filters.extend(TEMPLATED_FILTERS)
+
+    print 'Processing as template: %s' % ', '.join(templated_filters)
 
     try:
         os.makedirs(destination_path)
@@ -53,12 +73,16 @@ def bootstrap(package_name, destination_path, template, charset):
             if os.path.isfile(src_fn):
                 f = file(src_fn)
                 try:
-                    tmpl = Template(f.read().decode('utf-8'))
+                    data = f.read()
                 finally:
                     f.close()
-                f = file(dst_fn, 'w')
+                for pat in templated_filters:
+                    if fnmatch(src_fn, pat):
+                        tmpl = Template(data.decode('utf-8'))
+                        data = tmpl.render(context).encode(charset)
+                f = file(dst_fn, 'wb')
                 try:
-                    f.write(tmpl.render(context).encode(charset))
+                    f.write(data)
                 finally:
                     f.close()
                 print '   ' + dst_fn
@@ -86,6 +110,9 @@ def bootstrap(package_name, destination_path, template, charset):
 
 
 def main(args):
+    """
+    Helper function for the `werkzeug-bootstrapping` script.
+    """
     usage = 'Usage: %s [-t <template>] [-c <charset>] ' \
             '<package> <dst>' % os.path.basename(args[0])
     try:
