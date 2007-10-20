a07954a543e7b7902573fdfa32be9312dd534b70
Armin Ronacher <armin.ronacher@active-4.com>
2007-10-20 17:09:10 +0200

    removed newline before head
    --HG--
    branch : trunk

--- werkzeug/test.py
+++ werkzeug/test.py
@@ -167,7 +167,6 @@ class Client(object):
         kw['method'] = 'POST'
         return self.open(*args, **kw)
 
-
     def head(self, *args, **kw):
         """Like open but method is enforced to HEAD"""
         kw['method'] = 'HEAD'
