8e7af078dbe5c9a72434ea9926d941e6e15c9377
Armin Ronacher <armin.ronacher@active-4.com>
2007-11-27 21:51:39 +0100

    added contrib as feature to setup.py
    --HG--
    branch : trunk

--- setup.py
+++ setup.py
@@ -5,20 +5,20 @@ import ez_setup
 from inspect import getdoc
 ez_setup.use_setuptools()
 
-from setuptools import setup, Extension, Feature
+from setuptools import setup, Feature
 
 setup(
-    name = 'Werkzeug',
-    version = '0.1',
-    url = 'http://werkzeug.pocoo.org/',
-    download_url = 'http://trac.pocoo.org/repos/werkzeug/trunk',
-    license = 'BSD',
-    author = 'Armin Ronacher',
-    author_email = 'armin.ronacher@active-4.com',
-    description = 'WSGI Utility Collection -- formal successor of Colubrid',
-    long_description = getdoc(werkzeug),
-    zip_safe = False,
-    classifiers = [
+    name='Werkzeug',
+    version='0.1',
+    url='http://werkzeug.pocoo.org/',
+    download_url='http://trac.pocoo.org/repos/werkzeug/trunk',
+    license='BSD',
+    author='Armin Ronacher',
+    author_email='armin.ronacher@active-4.com',
+    description='WSGI Utility Collection -- formal successor of Colubrid',
+    long_description=getdoc(werkzeug),
+    zip_safe=False,
+    classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
@@ -28,11 +28,17 @@ setup(
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ],
-    scripts = ['werkzeug-bootstrap'],
-    packages = ['werkzeug', 'werkzeug.debug'],
-    platforms = 'any',
-    include_package_data = True,
-    extras_require = {
+    scripts=['werkzeug-bootstrap'],
+    packages=['werkzeug', 'werkzeug.debug'],
+    features={
+        'contrib': Feature('optional contribute addon modules',
+            standard=True,
+            packages=['werkzeug.contrib']
+        )
+    },
+    platforms='any',
+    include_package_data=True,
+    extras_require={
         'plugin': ['setuptools>=0.6a2'],
         'wsgiref': ['wsgiref']
     }
