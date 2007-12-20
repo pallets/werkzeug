218c6f9a75059be4b735019515391c1142a76ace
Marek Kubica <pythonmailing@web.de>
2007-12-21 00:30:06 +0100

    Implemented optional session handling for kickstart
    --HG--
    branch : trunk

--- werkzeug/contrib/kickstart.py
+++ werkzeug/contrib/kickstart.py
@@ -14,18 +14,32 @@ from os import path
 from werkzeug.wrappers import BaseRequest, BaseResponse
 from werkzeug.templates import Template
 
-
 __all__ = ['Request', 'Response', 'TemplateNotFound', 'TemplateLoader']
 
 
 class Request(BaseRequest):
     """
     A handy subclass of the base request that adds a URL builder.
+    It when supplied a session store, it is also able to handle sessions.
     """
 
-    def __init__(self, environ, url_map):
+    def __init__(self, environ, url_map,
+            session_store=None, cookie_name=None):
+        # call the parent for initialization
         BaseRequest.__init__(self, environ)
+        # create an adapter
         self.url_adapter = url_map.bind_to_environ(environ)
+        # create all stuff for sessions
+        self.session_store = session_store
+        self.cookie_name = cookie_name
+
+        if session_store is not None and cookie_name is not None:
+            if cookie_name in self.cookies:
+                # get the session out of the storage
+                self.session = session_store.get(self.cookies[cookie_name])
+            else:
+                # create a new session
+                self.session = session_store.new()
 
     def url_for(self, callback, **values):
         return self.url_adapter.build(callback, values)
@@ -33,10 +47,26 @@ class Request(BaseRequest):
 
 class Response(BaseResponse):
     """
-    A subclass of base response which sets the default mimetype to text/html
+    A subclass of base response which sets the default mimetype to text/html.
+    It the `Request` that came in is using Werkzeug sessions, this class
+    takes care of saving that session.
     """
     default_mimetype = 'text/html'
 
+    def __call__(self, environ, start_response):
+        # get the request object
+        request = environ['werkzeug.request']
+
+        if request.session_store is not None:
+            # save the session if neccessary
+            request.session_store.save_if_modified(request.session)
+
+            # set the cookie for the browser if it is not there:
+            if request.cookie_name not in request.cookies:
+                self.set_cookie(request.cookie_name, request.session.sid)
+    
+        # go on with normal response business
+        return BaseResponse.__call__(self, environ, start_response)
 
 class TemplateNotFound(IOError, LookupError):
     """
