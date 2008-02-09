9c1842132e3d595a9fdc696d444a5524a4c9a750
Armin Ronacher <armin.ronacher@active-4.com>
2008-02-09 12:53:36 +0100

    updated readme
    --HG--
    branch : trunk

--- examples/manage-cupoftee.py
+++ examples/manage-cupoftee.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python
+"""
+    Manage Cup Of Tee
+    ~~~~~~~~~~~~~~~~~
+
+    Manage the cup of tee application.
+
+    :copyright: 2008 by Armin Ronacher.
+    :license: BSD, see LICENSE for more details.
+"""
 from cupoftee import make_app
 from werkzeug import script
 
