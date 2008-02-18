136447d2ce4982b3055b8f269a258a1ceb5a1995
Armin Ronacher <armin.ronacher@active-4.com>
2008-02-18 15:24:57 +0100

    no comment

--- werkzeug/contrib/cache.py
+++ werkzeug/contrib/cache.py
@@ -24,7 +24,7 @@ from cPickle import loads, dumps, load, dump, HIGHEST_PROTOCOL
 
 have_memcache = True
 try:
-    import cmemcache import memcache
+    import cmemcache as memcache
 except ImportError:
     try:
         import memcache
