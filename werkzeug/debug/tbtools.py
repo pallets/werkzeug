043572e157e944b9702b1c77ef0ac9cf4a4ea38c
Armin Ronacher <armin.ronacher@active-4.com>
2008-06-27 19:56:28 +0200

    Traceback supports latest lodgeit changes now

--- werkzeug/debug/tbtools.py
+++ werkzeug/debug/tbtools.py
@@ -142,7 +142,7 @@ class Traceback(object):
         from xmlrpclib import ServerProxy
         srv = ServerProxy('http://paste.pocoo.org/xmlrpc/')
         return srv.pastes.newPaste('pytb', self.plaintext)
-        return '{"url": "http://paste.pocoo.org/show/%d/", "id": %d}' % \
+        return '{"url": "http://paste.pocoo.org/show/%s/", "id": %s}' % \
                (paste_id, paste_id)
 
     def render_summary(self, include_title=True):
