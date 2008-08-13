52607c19b1aca63dad9be9acd48edb00d39782d1
Armin Ronacher <armin.ronacher@active-4.com>
2008-08-13 20:23:40 +0200

    Added support for RFC 2324

--- werkzeug/_internal.py
+++ werkzeug/_internal.py
@@ -60,6 +60,7 @@ HTTP_STATUS_CODES = {
     415:    'Unsupported Media Type',
     416:    'Requested Range Not Satisfiable',
     417:    'Expectation Failed',
+    418:    'I\'m a teapot',
     422:    'Unprocessable Entity',
     423:    'Locked',
     424:    'Failed Dependency',
