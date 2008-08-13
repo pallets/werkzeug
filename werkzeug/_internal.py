87872741c5bccad368da28f0bb8637f76a070e8d
Armin Ronacher <armin.ronacher@active-4.com>
2008-08-13 20:25:11 +0200

    Added comment for latest change

--- werkzeug/_internal.py
+++ werkzeug/_internal.py
@@ -60,7 +60,7 @@ HTTP_STATUS_CODES = {
     415:    'Unsupported Media Type',
     416:    'Requested Range Not Satisfiable',
     417:    'Expectation Failed',
-    418:    'I\'m a teapot',
+    418:    'I\'m a teapot',        # see RFC 2324
     422:    'Unprocessable Entity',
     423:    'Locked',
     424:    'Failed Dependency',
