fd55b277162fa7fb48b6b2d24f47130fc7c53d3f
Armin Ronacher <armin.ronacher@active-4.com>
2009-04-24 16:41:48 +0200

    Added an information about dump() to the debug page.

--- werkzeug/debug/shared/debugger.js
+++ werkzeug/debug/shared/debugger.js
@@ -74,11 +74,15 @@ $(function() {
    */
   $('span.nojavascript')
     .removeClass('nojavascript')
-    .text('To switch between the interactive traceback and the plaintext ' +
+    .html('<p>To switch between the interactive traceback and the plaintext ' +
           'one, you can click on the "Traceback" headline.  From the text ' +
           'traceback you can also create a paste of it.  For code execution ' +
           'mouse-over the frame you want to debug and click on the console ' +
-          'icon on the right side.');
+          'icon on the right side.' +
+          '<p>You can execute arbitrary Python code in the stack frames and ' +
+          'there are some extra helpers available for introspection:' +
+          '<ul><li><code>dump()</code> shows all variables in the frame' +
+          '<li><code>dump(obj)</code> dumps all what\'s know about the object</ul>');
 
   /**
    * Add the pastebin feature
