ceb3eddf6fc75f8cc46f4cef03d351c9c98bb29d
Armin Ronacher <armin.ronacher@active-4.com>
2009-07-09 22:24:24 +0200

    tip is 0.6

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ except ImportError:
 
 setup(
     name='Werkzeug',
-    version='0.5.1',
+    version='0.6',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
