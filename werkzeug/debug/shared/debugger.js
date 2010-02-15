d9a2fe3a0e1f6a395dba128905e52a4390709443
Armin Ronacher <armin.ronacher@active-4.com>
2010-02-15 21:17:27 +0100

    Do not show the explanation for the debugger if the debugger is not enabled.  This fixes #335.

--- werkzeug/debug/shared/debugger.js
+++ werkzeug/debug/shared/debugger.js
@@ -76,13 +76,13 @@ $(function() {
     .removeClass('nojavascript')
     .html('<p>To switch between the interactive traceback and the plaintext ' +
           'one, you can click on the "Traceback" headline.  From the text ' +
-          'traceback you can also create a paste of it.  For code execution ' +
-          'mouse-over the frame you want to debug and click on the console ' +
-          'icon on the right side.' +
+          'traceback you can also create a paste of it. ' + (!EVALEX ? '' :
+          'For code execution mouse-over the frame you want to debug and ' +
+          'click on the console icon on the right side.' +
           '<p>You can execute arbitrary Python code in the stack frames and ' +
           'there are some extra helpers available for introspection:' +
           '<ul><li><code>dump()</code> shows all variables in the frame' +
-          '<li><code>dump(obj)</code> dumps all that\'s known about the object</ul>');
+          '<li><code>dump(obj)</code> dumps all that\'s known about the object</ul>'));
 
   /**
    * Add the pastebin feature
