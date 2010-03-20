aa9909718f0e7d5308187a2f520c56e4c2012e6a
Christopher Grebs <cg@webshox.org>
2010-03-20 13:10:40 +0100

    fixed #499

--- werkzeug/_internal.py
+++ werkzeug/_internal.py
@@ -107,7 +107,9 @@ def _log(type, message, *args, **kwargs):
     if _logger is None:
         import logging
         _logger = logging.getLogger('werkzeug')
-        if _logger.level == logging.NOTSET:
+        # Only set up a default log handler if the
+        # end-user application didn't set anything up.
+        if not logging.root.handlers and _logger.level == logging.NOTSET:
             _logger.setLevel(logging.INFO)
             handler = logging.StreamHandler()
             _logger.addHandler(handler)
