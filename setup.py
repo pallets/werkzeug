2c548742eaf638346eefb5dac9c78626d84c6b70
Armin Ronacher <armin.ronacher@active-4.com>
2011-07-25 13:47:18 +0200

    This is 0.7.1-dev

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except ImportError:
 
 setup(
     name='Werkzeug',
-    version='0.7',
+    version='0.7.1-dev',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
