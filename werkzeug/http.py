8d4358a2542b42d1539950ea6379c93a19f17f05
Joshua Bronson <jabronson@gmail.com>
2011-08-23 08:37:08 -0300

    It's the "Trailer" header, not "Trailers"

--- werkzeug/http.py
+++ werkzeug/http.py
@@ -52,7 +52,7 @@ _entity_headers = frozenset([
 ])
 _hop_by_pop_headers = frozenset([
     'connection', 'keep-alive', 'proxy-authenticate',
-    'proxy-authorization', 'te', 'trailers', 'transfer-encoding',
+    'proxy-authorization', 'te', 'trailer', 'transfer-encoding',
     'upgrade'
 ])
 
