6d4d856dc950865951b623d5a73108633b0f26f0
Armin Ronacher <armin.ronacher@active-4.com>
2011-09-30 00:49:21 +0200

    Bumped version to 0.8.1

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except ImportError:
 
 setup(
     name='Werkzeug',
-    version='0.8',
+    version='0.8.1',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
