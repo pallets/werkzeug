ef824aa8e0bd3f59e716f98b5cb63cb2ef209628
Markus Unterwaditzer <markus@unterwaditzer.net>
2013-03-11 22:53:25 +0100

    Made stuff more readable (?)

--- werkzeug/_compat.py
+++ werkzeug/_compat.py
@@ -7,9 +7,14 @@ except ImportError:
 import six
 from functools import partial
 
-iterkeys = lambda d, *a, **kw: iter(getattr(d, six._iterkeys)(*a, **kw))
-itervalues = lambda d, *a, **kw: iter(getattr(d, six._itervalues)(*a, **kw))
-iteritems = lambda d, *a, **kw: iter(getattr(d, six._iteritems)(*a, **kw))
+def iterkeys(d, *a, **kw):
+    return iter(getattr(d, six._iterkeys)(*a, **kw))
+
+def itervalues(d, *a, **kw):
+    return iter(getattr(d, six._itervalues)(*a, **kw))
+
+def iteritems(d, *a, **kw):
+    return iter(getattr(d, six._iteritems)(*a, **kw))
 
 dict_iterkeys = partial(iterkeys, dict)
 dict_itervalues = partial(itervalues, dict)
