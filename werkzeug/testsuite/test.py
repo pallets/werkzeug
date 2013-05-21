905378a187397980b430efd871daf69ec89a07b3
Daniel Neuhäuser <dasdasich@gmail.com>
2013-05-21 15:00:44 +0200

    Don't leak fd in test_environ_builder_stream_switch

--- werkzeug/testsuite/test.py
+++ werkzeug/testsuite/test.py
@@ -216,6 +216,7 @@ class TestTestCase(WerkzeugTestCase):
                                     'CONTENT_TYPE': 'multipart/form-data; boundary="%s"' %
                                     boundary})[1]
             self.assert_strict_equal(form, d)
+            stream.close()
 
     def test_create_environ(self):
         env = create_environ('/foo?bar=baz', 'http://example.org/')
