cd0cc209c4426bf8017bbaedead070bd51da0bd4
Daniel Neuhäuser <dasdasich@gmail.com>
2013-05-23 18:33:53 +0200

    Fix escaped str vs. bytes issue in quote

--- werkzeug/urls.py
+++ werkzeug/urls.py
@@ -158,7 +158,7 @@ def quote(string, safe='/', charset='utf-8', errors='strict'):
     ))
     return b''.join(
         (int2byte(char) if isinstance(char, int) else char)
-        if char in safe else '%%%x' % char
+        if char in safe else ('%%%x' % char).encode('ascii')
         for char in string
     )
 
