e0c2eb4f86191274dc0199942b95a42d89138c39
Armin Ronacher <armin.ronacher@active-4.com>
2013-06-14 09:52:00 +0100

    In case we need a 0.9.2

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except ImportError:
 
 setup(
     name='Werkzeug',
-    version='0.9.1',
+    version='0.9.2-dev',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
