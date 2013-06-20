d9900c2812e22f7ab65e19e89506603698fd213a
Markus Unterwaditzer <markus@unterwaditzer.net>
2013-06-20 21:45:26 +0200

    Add more tests

--- werkzeug/testsuite/contrib/cache.py
+++ werkzeug/testsuite/contrib/cache.py
@@ -42,8 +42,8 @@ class SimpleCacheTestCase(WerkzeugTestCase):
 
     def test_get_dict(self):
         c = cache.SimpleCache()
-        c.set('a', 'a')
-        c.set('b', 'b')
+        assert c.set('a', 'a')
+        assert c.set('b', 'b')
         d = c.get_dict('a', 'b')
         assert 'a' in d
         assert 'a' == d['a']
@@ -52,9 +52,9 @@ class SimpleCacheTestCase(WerkzeugTestCase):
 
     def test_set_many(self):
         c = cache.SimpleCache()
-        c.set_many({0: 0, 1: 1, 2: 4})
+        assert c.set_many({0: 0, 1: 1, 2: 4})
         assert c.get(2) == 4
-        c.set_many((i, i*i) for i in range(3))
+        assert c.set_many((i, i*i) for i in range(3))
         assert c.get(2) == 4
 
 
@@ -65,7 +65,7 @@ class FileSystemCacheTestCase(WerkzeugTestCase):
         try:
             c = cache.FileSystemCache(cache_dir=tmp_dir)
             for i in range(3):
-                c.set(str(i), i * i)
+                assert c.set(str(i), i * i)
             for i in range(3):
                 result = c.get(str(i))
                 assert result == i * i
@@ -77,7 +77,7 @@ class FileSystemCacheTestCase(WerkzeugTestCase):
         tmp_dir = tempfile.mkdtemp()
         c = cache.FileSystemCache(cache_dir=tmp_dir, threshold=THRESHOLD)
         for i in range(2 * THRESHOLD):
-            c.set(str(i), i)
+            assert c.set(str(i), i)
         cache_files = os.listdir(tmp_dir)
         shutil.rmtree(tmp_dir)
         assert len(cache_files) <= THRESHOLD
@@ -86,10 +86,10 @@ class FileSystemCacheTestCase(WerkzeugTestCase):
     def test_filesystemcache_clear(self):
         tmp_dir = tempfile.mkdtemp()
         c = cache.FileSystemCache(cache_dir=tmp_dir)
-        c.set('foo', 'bar')
+        assert c.set('foo', 'bar')
         cache_files = os.listdir(tmp_dir)
         assert len(cache_files) == 1
-        c.clear()
+        assert c.clear()
         cache_files = os.listdir(tmp_dir)
         assert len(cache_files) == 0
         shutil.rmtree(tmp_dir)
@@ -105,69 +105,69 @@ class RedisCacheTestCase(WerkzeugTestCase):
 
     def test_compat(self):
         c = self.make_cache()
-        c._client.set(c.key_prefix + 'foo', b'Awesome')
-        self.assert_equal(c.get('foo'), b'Awesome')
-        c._client.set(c.key_prefix + 'foo', b'42')
+        assert c._client.set(c.key_prefix + 'foo', 'Awesome')
+        self.assert_equal(c.get('foo'), 'Awesome')
+        assert c._client.set(c.key_prefix + 'foo', '42')
         self.assert_equal(c.get('foo'), 42)
 
     def test_get_set(self):
         c = self.make_cache()
-        c.set('foo', ['bar'])
+        assert c.set('foo', ['bar'])
         assert c.get('foo') == ['bar']
 
     def test_get_many(self):
         c = self.make_cache()
-        c.set('foo', ['bar'])
-        c.set('spam', 'eggs')
+        assert c.set('foo', ['bar'])
+        assert c.set('spam', 'eggs')
         assert c.get_many('foo', 'spam') == [['bar'], 'eggs']
 
     def test_set_many(self):
         c = self.make_cache()
-        c.set_many({'foo': 'bar', 'spam': ['eggs']})
+        assert c.set_many({'foo': 'bar', 'spam': ['eggs']})
         assert c.get('foo') == 'bar'
         assert c.get('spam') == ['eggs']
 
     def test_expire(self):
         c = self.make_cache()
-        c.set('foo', 'bar', 1)
+        assert c.set('foo', 'bar', 1)
         time.sleep(2)
         assert c.get('foo') is None
 
     def test_add(self):
         c = self.make_cache()
         # sanity check that add() works like set()
-        c.add('foo', 'bar')
+        assert c.add('foo', 'bar')
         assert c.get('foo') == 'bar'
-        c.add('foo', 'qux')
+        assert c.add('foo', 'qux')
         assert c.get('foo') == 'bar'
 
     def test_delete(self):
         c = self.make_cache()
-        c.add('foo', 'bar')
+        assert c.add('foo', 'bar')
         assert c.get('foo') == 'bar'
-        c.delete('foo')
+        assert c.delete('foo')
         assert c.get('foo') is None
 
     def test_delete_many(self):
         c = self.make_cache()
-        c.add('foo', 'bar')
-        c.add('spam', 'eggs')
-        c.delete_many('foo', 'spam')
+        assert c.add('foo', 'bar')
+        assert c.add('spam', 'eggs')
+        assert c.delete_many('foo', 'spam')
         assert c.get('foo') is None
         assert c.get('spam') is None
 
     def test_inc_dec(self):
         c = self.make_cache()
-        c.set('foo', 1)
-        self.assert_equal(c.inc('foo'), 2)
-        self.assert_equal(c.dec('foo'), 1)
-        c.delete('foo')
+        assert c.set('foo', 1)
+        assert c.inc('foo') == 2
+        assert c.dec('foo') == 1
+        assert c.delete('foo')
 
     def test_true_false(self):
         c = self.make_cache()
-        c.set('foo', True)
+        assert c.set('foo', True)
         assert c.get('foo') == True
-        c.set('bar', False)
+        assert c.set('bar', False)
         assert c.get('bar') == False
 
 
