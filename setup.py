ea743cb8d9831d1270fda484657a0885fc3dc521
Armin Ronacher <armin.ronacher@active-4.com>
2013-07-18 16:08:38 +0100

    This is ready for 0.9.3

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ except ImportError:
 
 setup(
     name='Werkzeug',
-    version='0.9.2',
+    version='0.9.3-dev',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
