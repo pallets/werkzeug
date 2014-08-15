4869f31e8a3e638edbf7458f3abd4ccf118eae7a
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-15 19:09:52 +0200

    Remove class in test_sessions

--- tests/contrib/test_sessions.py
+++ tests/contrib/test_sessions.py
@@ -17,62 +17,60 @@ from tests import WerkzeugTests
 from werkzeug.contrib.sessions import FilesystemSessionStore
 
 
-class TestSession(WerkzeugTests):
+def test_default_tempdir():
+    store = FilesystemSessionStore()
+    assert store.path == gettempdir()
 
-    def test_default_tempdir(self):
-        store = FilesystemSessionStore()
-        assert store.path == gettempdir()
+def test_basic_fs_sessions(tmpdir):
+    store = FilesystemSessionStore(str(tmpdir))
+    x = store.new()
+    assert x.new
+    assert not x.modified
+    x['foo'] = [1, 2, 3]
+    assert x.modified
+    store.save(x)
 
-    def test_basic_fs_sessions(self, tmpdir):
+    x2 = store.get(x.sid)
+    assert not x2.new
+    assert not x2.modified
+    assert x2 is not x
+    assert x2 == x
+    x2['test'] = 3
+    assert x2.modified
+    assert not x2.new
+    store.save(x2)
+
+    x = store.get(x.sid)
+    store.delete(x)
+    x2 = store.get(x.sid)
+    # the session is not new when it was used previously.
+    assert not x2.new
+
+def test_non_urandom(tmpdir):
+    urandom = os.urandom
+    del os.urandom
+    try:
         store = FilesystemSessionStore(str(tmpdir))
-        x = store.new()
-        assert x.new
-        assert not x.modified
-        x['foo'] = [1, 2, 3]
-        assert x.modified
-        store.save(x)
-
-        x2 = store.get(x.sid)
-        assert not x2.new
-        assert not x2.modified
-        assert x2 is not x
-        assert x2 == x
-        x2['test'] = 3
-        assert x2.modified
-        assert not x2.new
-        store.save(x2)
-
-        x = store.get(x.sid)
-        store.delete(x)
-        x2 = store.get(x.sid)
-        # the session is not new when it was used previously.
-        assert not x2.new
-
-    def test_non_urandom(self, tmpdir):
-        urandom = os.urandom
-        del os.urandom
-        try:
-            store = FilesystemSessionStore(str(tmpdir))
-            store.new()
-        finally:
-            os.urandom = urandom
+        store.new()
+    finally:
+        os.urandom = urandom
 
 
-    def test_renewing_fs_session(self, tmpdir):
-        store = FilesystemSessionStore(str(tmpdir), renew_missing=True)
-        x = store.new()
-        store.save(x)
-        store.delete(x)
-        x2 = store.get(x.sid)
-        assert x2.new
+def test_renewing_fs_session(tmpdir):
+    store = FilesystemSessionStore(str(tmpdir), renew_missing=True)
+    x = store.new()
+    store.save(x)
+    store.delete(x)
+    x2 = store.get(x.sid)
+    assert x2.new
 
-    def test_fs_session_lising(self, tmpdir):
-        store = FilesystemSessionStore(str(tmpdir), renew_missing=True)
-        sessions = set()
-        for x in range(10):
-            sess = store.new()
-            store.save(sess)
-            sessions.add(sess.sid)
+def test_fs_session_lising(tmpdir):
+    store = FilesystemSessionStore(str(tmpdir), renew_missing=True)
+    sessions = set()
+    for x in range(10):
+        sess = store.new()
+        store.save(sess)
+        sessions.add(sess.sid)
 
-        listed_sessions = set(store.list())
-        assert sessions == listed_sessions
+    listed_sessions = set(store.list())
+    assert sessions == listed_sessions
