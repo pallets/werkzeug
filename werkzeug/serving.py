8e701fb7cb5a2eaad274f21c0aadc307acaac697
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-24 17:02:18 +0200

    Code reuse

--- werkzeug/serving.py
+++ werkzeug/serving.py
@@ -355,8 +355,7 @@ def generate_adhoc_ssl_context():
         os.write(pkey_handle, crypto.dump_privatekey(crypto.FILETYPE_PEM, pkey))
         os.close(cert_handle)
         os.close(pkey_handle)
-        ctx = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-        ctx.load_cert_chain(cert_file, pkey_file)
+        ctx = load_ssl_context(cert_file, pkey_file)
     finally:
         try:
             os.unlink(pkey_file)
