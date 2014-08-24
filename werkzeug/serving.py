beeec6addd54dffd663c47e425dbf3a50b8beb43
Markus Unterwaditzer <markus@unterwaditzer.net>
2014-08-24 16:29:57 +0200

    Fix for PyPy

--- werkzeug/serving.py
+++ werkzeug/serving.py
@@ -391,12 +391,15 @@ def is_ssl_error(error=None):
 
 
 class _SSLConnectionFix(object):
-    """Wrapper around SSL connection to provide a working makefile()."""
+    """Wrapper around SSL connection to provide compatibility to the stdlib's
+    socket module."""
 
     def __init__(self, con):
         self._con = con
+        self._pypy_refcounter = 0
 
     def makefile(self, mode, bufsize):
+        # OpenSSL simply doesn't implement it.
         return SocketIO(self, mode)
 
     def __getattr__(self, attrib):
@@ -407,6 +410,24 @@ class _SSLConnectionFix(object):
             self._con.shutdown()
         except Exception:
             pass
+
+    # From the comments of PyPy's Python 2 ``socket._fileobject.__init__``:
+    #
+    # >Note that a few libraries (like eventlet) poke at the
+    # >private implementation of socket.py, passing custom
+    # >objects to _fileobject().  These libraries need the
+    # >following fix for use on PyPy: the custom objects need
+    # >methods _reuse() and _drop() that maintains an explicit
+    # >reference counter, starting at 0.  When it drops back to
+    # >zero, close() must be called.
+
+    def _drop(self):
+        self._pypy_refcounter -= 1
+        if self._pypy_refcounter == 0:
+            self.close()
+
+    def _reuse(self):
+        self._pypy_refcounter += 1
 
     try:
         buffer
