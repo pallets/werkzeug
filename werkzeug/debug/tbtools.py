pallets/werkzeug

commit 4381d6eff5e84f09d5f48d1ed79a3533c9030726

Fix regex for determining charset in traceback
Fix #547

werkzeug/debug/tbtools.py
@@ -479,7 +479,7 @@ class Frame(object):
             source = source[3:]
         else:
             for idx, match in enumerate(_line_re.finditer(source)):
-                match = _line_re.search(match.group())
+                match = _coding_re.search(match.group())
                 if match is not None:
                     charset = match.group(1)
                     break

werkzeug/testsuite/debug.py
@@ -10,6 +10,7 @@
 """
 import unittest
 import sys
+import os
 import re
 import io
 
@@ -183,6 +184,32 @@ class TracebackTestCase(WerkzeugTestCase):
         traceback.log(buffer_)
         self.assert_equal(buffer_.getvalue().strip(),
                           traceback.plaintext.strip())
+
+    def test_sourcelines_encoding(self):
+        source = (u'# -*- coding: latin1 -*-\n\n'
+                  u'def foo():\n'
+                  u'    """höhö"""\n'
+                  u'    1 / 0\n'
+                  u'foo()').encode('latin1')
+        code = compile(source, filename='lol.py', mode='exec')
+        try:
+            eval(code)
+        except ZeroDivisionError:
+            tb = sys.exc_info()[2]
+            traceback = Traceback(*sys.exc_info())
+
+        frames = traceback.frames
+        self.assert_equal(len(frames), 3)
+        self.assert_equal(frames[1].filename, 'lol.py')
+        self.assert_equal(frames[2].filename, 'lol.py')
+
+        class Loader(object):
+            def get_source(self, module):
+                return source
+
+        frames[1].loader = frames[2].loader = Loader()
+        self.assert_equal(frames[1].sourcelines, frames[2].sourcelines)
+        self.assert_in(u'höhö', frames[1].sourcelines[3])
 
 
 def suite():

commit 3298dadc60d3506a0c49efbcb02b12d48be21039

Fix regression in tbtools re Python 3
Introduced in 4381d6eff5e84f09d5f48d1ed79a3533c9030726

werkzeug/debug/tbtools.py
@@ -26,7 +26,7 @@ from werkzeug._compat import range_type, PY2, text_type, string_types, \
 
 _coding_re = re.compile(br'coding[:=]\s*([-\w.]+)')
 _line_re = re.compile(br'^(.*?)$(?m)')
-_funcdef_re = re.compile(br'^(\s*def\s)|(.*(?<!\w)lambda(:|\s))|^(\s*@)')
+_funcdef_re = re.compile(r'^(\s*def\s)|(.*(?<!\w)lambda(:|\s))|^(\s*@)')
 UTF8_COOKIE = b'\xef\xbb\xbf'
 
 system_exceptions = (SystemExit, KeyboardInterrupt)

werkzeug/testsuite/debug.py
@@ -207,6 +207,10 @@ class TracebackTestCase(WerkzeugTestCase):
 
         frames[1].loader = frames[2].loader = Loader()
         self.assert_equal(frames[1].sourcelines, frames[2].sourcelines)
+        self.assert_equal(
+            [line.code for line in frames[1].get_annotated_lines()],
+            [line.code for line in frames[2].get_annotated_lines()]
+        )
         self.assert_in(u'höhö', frames[1].sourcelines[3])
 
 