f88e865b3915cfea88b438683b5632554f9b9304
Josh Friend <jfriend@michiganlabs.com>
2015-02-01 00:12:48 -0500

    fix regression from #660

--- tests/test_routing.py
+++ tests/test_routing.py
@@ -439,6 +439,16 @@ def test_build_append_multiple():
     assert a == 'http://example.org/bar/0.815'
     assert set(b.split('&')) == set('pof=2.0&bif=1.0&bif=3.0'.split('&'))
 
+    params = {
+        'bazf': 0.815,
+        'bif': [1.0, 3.0],
+        'pof': 2.0,
+    }
+    a, b = adapter.build('barf', params).split('?')
+    assert a == 'http://example.org/bar/0.815'
+    assert set(b.split('&')) == set('pof=2.0&bif=1.0&bif=3.0'.split('&'))
+
+
 def test_method_fallback():
     map = r.Map([
         r.Rule('/', endpoint='index', methods=['GET']),
