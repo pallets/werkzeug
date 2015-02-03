0f6a7cc48d6bf403a5cc390937cbed49fe1f13d7
Armin Ronacher <armin.ronacher@active-4.com>
2015-02-03 23:00:49 +0100

    In case we need a 0.10.2-dev

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ class TestCommand(Command):
 
 setup(
     name='Werkzeug',
-    version='0.10.1',
+    version='0.10.2-dev',
     url='http://werkzeug.pocoo.org/',
     license='BSD',
     author='Armin Ronacher',
