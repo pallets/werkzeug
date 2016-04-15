1eb4912ee919fef165c921dafefb0f729003b4fa
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-04-15 15:19:33 +0200

    Revert "Add tests for get_machine_id"
    This reverts commit 1581b8d770b1e685f54f62385528c352f629f6bf.

--- tests/test_debug.py
+++ tests/test_debug.py
@@ -12,7 +12,6 @@ import sys
 import re
 import io
 
-from werkzeug.debug import get_machine_id
 from werkzeug.debug.repr import debug_repr, DebugReprGenerator, \
     dump, helper
 from werkzeug.debug.console import HTMLStringO
@@ -234,8 +233,3 @@ class TestTraceback(object):
             traceback = Traceback(*sys.exc_info())
 
         assert u'föö' in u'\n'.join(frame.render() for frame in traceback.frames)
-
-
-def test_get_machine_id():
-    rv = get_machine_id()
-    assert isinstance(rv, bytes)
