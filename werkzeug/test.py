62563b86765f35e1480c799296c539c6dcd70c63
Markus Unterwaditzer <markus@unterwaditzer.net>
2016-08-15 00:05:52 +0200

    Passing integers to test client in Python 3
    Fix #852
    Fix #894

--- werkzeug/test.py
+++ werkzeug/test.py
@@ -99,8 +99,8 @@ def stream_encode_multipart(values, use_tempfile=True, threshold=1024 * 500,
             else:
                 if not isinstance(value, string_types):
                     value = str(value)
-                else:
-                    value = to_bytes(value, charset)
+
+                value = to_bytes(value, charset)
                 write('\r\n\r\n')
                 write_binary(value)
             write('\r\n')
