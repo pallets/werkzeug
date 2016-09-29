ecc60c8ff8f1c90b0dff104e67a23d32bad12a17
Shadab Zafar <dufferzafar0@gmail.com>
2016-09-30 00:15:55 +0530

    Add termcolor to extras_require

--- setup.py
+++ setup.py
@@ -111,6 +111,7 @@ setup(
     packages=['werkzeug', 'werkzeug.debug', 'werkzeug.contrib'],
     extras_require={
         'watchdog': ['watchdog'],
+        'termcolor',
     },
     cmdclass=dict(test=TestCommand),
     include_package_data=True,
