3b88079b212ed7f674c06db5a0cfa5e663a4b800
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-03-10 12:26:46 +0100

    Bump to dev version

--- werkzeug/__init__.py
+++ werkzeug/__init__.py
@@ -19,7 +19,7 @@ import sys
 
 from werkzeug._compat import iteritems
 
-__version__ = '0.12'
+__version__ = '0.13-dev'
 
 
 # This import magic raises concerns quite often which is why the implementation
