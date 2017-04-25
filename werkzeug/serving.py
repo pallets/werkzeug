f792387d7405d14b21f85b5c1563ce83cc631086
eelis.takala <eelis.takala@trafotek.fi>
2017-04-25 12:41:29 +0300

    IOError error too broad as Jordan Pittier and Markus Unterwditzer pointed out. In order to narrow it down, added a check if the the error is a broken pipe.

--- werkzeug/serving.py
+++ werkzeug/serving.py
@@ -209,8 +209,10 @@ class WSGIRequestHandler(BaseHTTPRequestHandler, object):
             execute(self.server.app)
         except (socket.error, socket.timeout) as e:
             self.connection_dropped(e, environ)
-        except IOError:
-            self.server.log('error', 'IOError: possibly a broken connection.')
+        except IOError, e:
+            import errno
+            if e.errno == errno.EPIPE:
+                self.server.log('error', 'IOError, Broken Pipe: possibly a broken connection.')
         except Exception:
             if self.server.passthrough_errors:
                 raise
