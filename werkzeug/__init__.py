fe37756adc77e46f7753eeeaf8dc0b34394951e2
Markus Unterwaditzer <markus@unterwaditzer.net>
2017-05-16 08:37:39 +0200

    Bump version number to 0.12.2

--- werkzeug/__init__.py
+++ werkzeug/__init__.py
@@ -19,7 +19,7 @@ import sys
 
 from werkzeug._compat import iteritems
 
-__version__ = '0.12.2-dev'
+__version__ = '0.12.2'
 
 
 # This import magic raises concerns quite often which is why the implementation
