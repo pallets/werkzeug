fcf6a06e4cf59e0899050f6d567479d90e8f9bf3
Jan Michael Auer <mail@jauer.org>
2017-11-28 12:01:45 +0100

    Improve DechunkedInput implementation and add comments
     - Try to read multiple chunks until the buffer is filled
     - Add comments to make control flow more clear
     - Add doc comment for DechunkedInput

--- werkzeug/serving.py
+++ werkzeug/serving.py
@@ -99,6 +99,8 @@ can_open_by_fd = not WIN and hasattr(socket, 'fromfd')
 
 
 class DechunkedInput(io.RawIOBase):
+    """An input stream that handles Transfer-Encoding 'chunked'"""
+
     def __init__(self, rfile):
         self._rfile = rfile
         self._done = False
@@ -107,31 +109,46 @@ class DechunkedInput(io.RawIOBase):
     def readable(self):
         return True
 
-    def readinto(self, buf):
-        if self._done:
-            return 0
-
-        if self._len == 0:
-            try:
-                self._len = int(self._rfile.readline().decode('latin1').strip(), 16)
-            except ValueError:
-                raise IOError('Invalid chunk header')
-
-        if self._len < 0:
+    def read_chunk_len(self):
+        try:
+            line = self._rfile.readline().decode('latin1')
+            _len = int(line.strip(), 16)
+        except ValueError:
+            raise IOError('Invalid chunk header')
+        if _len < 0:
             raise IOError('Negative chunk length not allowed')
-        elif self._len > 0:
-            n = min(len(buf), self._len)
-            buf[:n] = self._rfile.read(n)
-            self._len -= n
-        else:
-            self._done = True
-            n = 0
+        return _len
 
-        if self._len == 0:
-            if self._rfile.readline() not in (b'\n', b'\r\n', b'\r'):
-                raise IOError('Missing chunk spacing')
+    def readinto(self, buf):
+        read = 0
+        while not self._done and read < len(buf):
+            if self._len == 0:
+                # This is the first chunk or we fully consumed the previous
+                # one. Read the next length of the next chunk
+                self._len = self.read_chunk_len()
 
-        return n
+            if self._len == 0:
+                # Found the final chunk of size 0. The stream is now exhausted,
+                # but there is still a final newline that should be consumed
+                self._done = True
+
+            if self._len > 0:
+                # There is data (left) in this chunk, so append it to the
+                # buffer. If this operation fully consumes the chunk, this will
+                # reset self._len to 0.
+                n = min(len(buf), self._len)
+                buf[read:read + n] = self._rfile.read(n)
+                self._len -= n
+                read += n
+
+            if self._len == 0:
+                # Skip the terminating newline of a chunk that has been fully
+                # consumed. This also applies to the 0-sized final chunk
+                terminator = self._rfile.readline()
+                if terminator not in (b'\n', b'\r\n', b'\r'):
+                    raise IOError('Missing chunk terminating newline')
+
+        return read
 
 
 class WSGIRequestHandler(BaseHTTPRequestHandler, object):
