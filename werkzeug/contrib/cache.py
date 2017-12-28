a1fff63c5ba5ae363d45b0fcd8ad8f49953e98ef
Reuben Cummings <reubano@users.noreply.github.com>
2017-12-29 00:15:14 +0300

    Convert dict.keys() to a list when calling get_multi (#1161)

--- werkzeug/contrib/cache.py
+++ werkzeug/contrib/cache.py
@@ -423,7 +423,8 @@ class MemcachedCache(BaseCache):
                 have_encoded_keys = True
             if _test_memcached_key(key):
                 key_mapping[encoded_key] = key
-        d = rv = self._client.get_multi(key_mapping.keys())
+        _keys = list(key_mapping)
+        d = rv = self._client.get_multi(_keys)
         if have_encoded_keys or self.key_prefix:
             rv = {}
             for key, value in iteritems(d):
