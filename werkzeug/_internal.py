4cf77d25858ff46ac7e9d64ade054bf05b41ce12
ramnes <contact@ramnes.eu>
2018-11-29 16:08:53 +0100

    Decorrelate Werkzeug's logger level setup from the presence of existing handlers
    For the rationale, see: https://github.com/pallets/flask/issues/2998

--- werkzeug/_internal.py
+++ werkzeug/_internal.py
@@ -78,10 +78,11 @@ def _log(type, message, *args, **kwargs):
     if _logger is None:
         import logging
         _logger = logging.getLogger('werkzeug')
+        if _logger.level == logging.NOTSET:
+            _logger.setLevel(logging.INFO)
         # Only set up a default log handler if the
         # end-user application didn't set anything up.
-        if not logging.root.handlers and _logger.level == logging.NOTSET:
-            _logger.setLevel(logging.INFO)
+        if not logging.root.handlers:
             handler = logging.StreamHandler()
             _logger.addHandler(handler)
     getattr(_logger, type)(message.rstrip(), *args, **kwargs)
