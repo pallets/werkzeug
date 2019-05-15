92ed359cea2ff8f558f48cdc19f709c24bef7545
David Lord <davidism@gmail.com>
2019-05-14 18:53:19 -0700

    start version 0.15.5

--- src/werkzeug/__init__.py
+++ src/werkzeug/__init__.py
@@ -17,7 +17,7 @@
 import sys
 from types import ModuleType
 
-__version__ = "0.15.4"
+__version__ = "0.15.5.dev0"
 
 # This import magic raises concerns quite often which is why the implementation
 # and motivation is explained here in detail now.
