1cc1afba893392e4ab66d5d7179802546591f892
Reece Dunham <me@rdil.rocks>
2019-10-31 13:16:39 +0000

    Fixed azure pipelines

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3 ONLY",
+        "Programming Language :: Python :: 3 Only",
         "Programming Language :: Python :: 3.5",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
