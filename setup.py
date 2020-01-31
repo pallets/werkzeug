e751e31bab28a80e83c5b4b6520979e553a61a10
pgjones <philip.graham.jones@googlemail.com>
2020-01-31 16:37:18 +0000

    Support for Python 3.4 has been dropped
    (This was missed previously)

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
-    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*",
+    python_requires=">=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*",
     extras_require={
         "watchdog": ["watchdog"],
         "dev": [
