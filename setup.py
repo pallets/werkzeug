51bbafdda6e03cf19bc7d2b725460e12e13578b8
David Lord <davidism@gmail.com>
2020-03-19 08:22:13 -0700

    move package metadata to setup.cfg

--- setup.py
+++ setup.py
@@ -1,54 +1,8 @@
-import re
-
-from setuptools import find_packages
 from setuptools import setup
 
-with open("README.rst", encoding="utf8") as f:
-    readme = f.read()
-
-with open("src/werkzeug/__init__.py", encoding="utf8") as f:
-    version = re.search(r'__version__ = "(.*?)"', f.read(), re.M).group(1)
-
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
 setup(
     name="Werkzeug",
-    version=version,
-    url="https://palletsprojects.com/p/werkzeug/",
-    project_urls={
-        "Documentation": "https://werkzeug.palletsprojects.com/",
-        "Code": "https://github.com/pallets/werkzeug",
-        "Issue tracker": "https://github.com/pallets/werkzeug/issues",
-    },
-    license="BSD-3-Clause",
-    author="Armin Ronacher",
-    author_email="armin.ronacher@active-4.com",
-    maintainer="Pallets",
-    maintainer_email="contact@palletsprojects.com",
-    description="The comprehensive WSGI web application library.",
-    long_description=readme,
-    classifiers=[
-        "Development Status :: 5 - Production/Stable",
-        "Environment :: Web Environment",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: BSD License",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: Implementation :: CPython",
-        "Programming Language :: Python :: Implementation :: PyPy",
-        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
-        "Topic :: Internet :: WWW/HTTP :: WSGI",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
-        "Topic :: Internet :: WWW/HTTP :: WSGI :: Middleware",
-        "Topic :: Software Development :: Libraries :: Application Frameworks",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-    ],
-    packages=find_packages("src"),
-    package_dir={"": "src"},
-    include_package_data=True,
-    python_requires=">=3.6",
     extras_require={
         "watchdog": ["watchdog"],
         "dev": [
