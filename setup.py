ddc9fef1bbc287de82a8bb6e41ebcaac0ef99690
David Lord <davidism@gmail.com>
2020-05-18 14:19:02 -0700

    use pip-compile to pin dev requirements

--- setup.py
+++ setup.py
@@ -1,18 +1,4 @@
 from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(
-    name="Werkzeug",
-    extras_require={
-        "watchdog": ["watchdog"],
-        "dev": [
-            "pytest",
-            "pytest-timeout",
-            "tox",
-            "sphinx",
-            "pallets-sphinx-themes",
-            "sphinxcontrib-log-cabinet",
-            "sphinx-issues",
-        ],
-    },
-)
+setup(name="Werkzeug", extras_require={"watchdog": ["watchdog"]})
