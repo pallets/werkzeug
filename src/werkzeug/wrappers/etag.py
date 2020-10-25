b8ed615b9ccd5d4a4f00b445bddf423f92755f58
Alireza Ayinmehr <Alireza.ayinmehr@gmail.com>
2020-10-26 01:47:03 +0330

    Explicitily specify that "Response.add_etag" adds etag based on the "Response.get_data"

--- src/werkzeug/wrappers/etag.py
+++ src/werkzeug/wrappers/etag.py
@@ -258,7 +258,7 @@ class ETagResponseMixin:
         return self  # type: ignore
 
     def add_etag(self, overwrite: bool = False, weak: bool = False) -> None:
-        """Add an etag for the current response if there is none yet."""
+        """Add an etag based on the current response data to current response, if there is none yet."""
         if overwrite or "etag" not in self.headers:
             self.set_etag(generate_etag(self.get_data()), weak)  # type: ignore
 
