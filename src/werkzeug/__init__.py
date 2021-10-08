7fa256705a4d69909d45c868d8b422495b9a9c23
Marty Ballard <marty331@gmail.com>
2021-10-07 23:07:25 -0500

    MultiDict ImportError fix
    Added missing import for MultiDict
    from .datastructures import MultiDict

--- src/werkzeug/__init__.py
+++ src/werkzeug/__init__.py
@@ -2,5 +2,6 @@ from .serving import run_simple as run_simple
 from .test import Client as Client
 from .wrappers import Request as Request
 from .wrappers import Response as Response
+from .datastructures import MultiDict
 
 __version__ = "2.1.0.dev0"
