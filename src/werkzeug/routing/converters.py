62fac4bbb6cee42d4858fe3ef40e82f366b354ca
Daniel Furman <djfurman@gmail.com>
2022-05-02 16:10:39 -0600

    AnyConverter validates the value to build

--- src/werkzeug/routing/converters.py
+++ src/werkzeug/routing/converters.py
@@ -81,13 +81,24 @@ class AnyConverter(BaseConverter):
     :param map: the :class:`Map`.
     :param items: this function accepts the possible items as positional
                   arguments.
+
+    .. versionchanged:: 2.2
+        Value is validated when building a URL.
     """
 
     part_isolating = True
 
     def __init__(self, map: "Map", *items: str) -> None:
         super().__init__(map)
+        self.items = set(items)
         self.regex = f"(?:{'|'.join([re.escape(x) for x in items])})"
+
+    def to_url(self, value: t.Any) -> str:
+        if value in self.items:
+            return str(value)
+
+        valid_values = ", ".join(f"'{item}'" for item in sorted(self.items))
+        raise ValueError(f"'{value}' is not one of {valid_values}")
 
 
 class PathConverter(BaseConverter):
